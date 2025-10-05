5a30b053d4e348fc40dbef4b2764f873ce5e6975
aliou konteye <aliou.konteye@gmail.com>
2025-10-05 20:34:42 +0200

    rails wikinimous done without design

--- Gemfile
+++ Gemfile
@@ -26,6 +26,11 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+# Gem Faker
+group :development, :test do
+  gem "faker"
+end
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+10.times do
+  new_article = Article.new(title: Faker::Name.name, content: Faker::Quote.famous_last_words)
+  new_article.save
+end
